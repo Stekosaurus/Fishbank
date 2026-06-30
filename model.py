cf9ceafe060a3965cfede801a6a3c1124ae7b14e
Stekosaurus <koch.steffen2@gmail.com>
2026-06-30 12:57:34 +0200

    zweiter Agent installiert

--- model.py
+++ model.py
@@ -14,18 +14,29 @@ class Fishbank(mesa.Model):
         super().__init__(seed=seed)
         self.ocean = Ocean.create_agents(model=self, n=1) #Initialisiert einen Ozean mit Fischen
         self.player = Player.create_agents (model=self, n=1) # Initialisiert die gewünschte anzahl Spieler(Aktuell nur 1).
+        self.opponent = Opponent.create_agents (model = self, n =1)
         self.ships = Ship.create_agents(model=self, n=1)#Initialisiert ein Schiff.
         #Initialisieren bedeuted das quasi aus der Vorlage der Klasse einzelne Objekte, Objekte mit denen Gearbeitet werden kann erstellt werden.
         
-        self.player[0].fleet.append(list(self.ships)[0])#Fügt ein Schiff der Flotte(fleet) des Spielers hinzu.
-
+        #self.player[0].fleet.append(list(self.ships)[0])#Fügt ein Schiff der Flotte(fleet) des Spielers hinzu.
+        #self.opponent[0].fleet.append
     def step(self):
         self.ocean.do("reproduce")
-        player = self.player[0]  # oder list(self.player)[0]
+        player = self.player[0]  
+        opponent = self.opponent[0]
+        self.opponent.do("execute")
+        for ship in opponent.fleet:
+            if ship.operating:
+                ship.catch()
+            else:
+                ship.idle()
         for ship in player.fleet:
             if ship.operating:
                 ship.catch()
             else:
                 ship.idle()
+        
+        #self
         self.player.do("sell_fish")
+        self.opponent.do("sell_fish")
             
